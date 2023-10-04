6de088828341853f2999e03940fb2da6e847e321
unknown <sergey2304s@mail.ru>
2023-10-04 23:01:58 +0300

    first commit

---
+++ src/main/java/com/example/marketplace/repository/BooksRepository.java
@@ -0,0 +1,9 @@
+package com.example.marketplace.repository;
+
+import com.example.marketplace.models.Books;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BooksRepository extends JpaRepository<Books, Integer> {
+}
